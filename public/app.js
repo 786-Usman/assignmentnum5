7d208bb74b2ca5d5f07c49803753060b7b677a23
786-usman <musmanmemon4@gmail.com>
2020-06-11 22:58:50 +0500

    assignment num 5

---
+++ public/app.js
@@ -0,0 +1,37 @@
+// Track 01
+alert("Error! Please enter a valid password.")
+
+
+
+var withBreaks = "Welcome to JS Land... \n Happy Coding!"
+ alert(withBreaks);
+
+
+alert("Welcome to JS Land...");
+    alert("Happy coding!");
+// alert("Prevent this page from creating additional dialogs.")
+
+
+
+ alert("Hellow... I can run JS though my web brower's console")
+
+
+
+// Track 02
+var username=("username")
+alert(username)
+
+var myname = ("Mohammad Usman")  
+alert(myname)
+
+
+var a = 26
+var b = "year old"
+var c = a + b
+alert(c)
+
+alert("Certified  Mobile Application development")
+
+
+var withBreaks = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+alert(withBreaks);
