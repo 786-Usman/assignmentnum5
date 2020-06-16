113e7fcdab4cf904d7c36b78e8bcb529fca54e88
786-usman <musmanmemon4@gmail.com>
2020-06-17 00:20:00 +0500

    assignmentnum5

--- public/app.js
+++ public/app.js
@@ -1,37 +1,447 @@
-// Track 01
-alert("Error! Please enter a valid password.")
+// ======   "Chapter 01" ====
+
+// ==== "chp 01 Task No 01"
+//  alert("Hello World!")
+
+ //  ==== "chp 01  Task No 02"
+// alert("Error! Please enter a valid password.")
+
+//   ==== " chp 01 Task No 03"
+// var withBreaks = "Welcome to JS Land... \n Happy Coding!"
+//  alert(withBreaks);
+ 
+
+//   ==== " chp 01 Task No 04"
+// alert("Welcome to JS Land...");
+//     alert("Happy coding!");
+
+
+//   ==== "  chp 01 Task No 05"
+//  alert("Hellow... I can run JS though my web brower's console")
+
+// console.log("Hellow... I can run JS though my web brower's console");
 
 
 
-var withBreaks = "Welcome to JS Land... \n Happy Coding!"
- alert(withBreaks);
+
+// ======   "Chapter 02" ====
+
+//   ==== " chp 02 Task No 01"
+// var username=("Username")
+// alert(username)
 
 
-alert("Welcome to JS Land...");
-    alert("Happy coding!");
-// alert("Prevent this page from creating additional dialogs.")
+// //   ==== " chp 02 Task No 02"
+// var myName = ("Mohammad Usman")  
+// alert(myName)
+
+//   ==== " chp 02 Task No 03"
+// var js=("Hello World")
+// alert(js)
+
+
+//   ==== " chp 02 Task No 04"
+// var name = ("Usman")
+// alert(name)
+
+// var a = 26
+// var b = "year old"
+
+// var c = a + b
+// alert(c)
+
+// alert("Certified  Mobile Application development")
+
+//   ==== " chp 02 Task No 05"
+
+// var withBreaks = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+// alert(withBreaks);
+
+//   ==== " chp 02  Task No 06"
+// var a ="My email address is  musmanmemon4@gmail.com"
+// alert(a)
+
+//   ==== " chp 02 Task No 07"
+// var book= "I am trying to learn from the Book \n A smarter way to learn JavaScript" ;
+// alert(book)
+
+// //   ==== "chp 02  Task No 08"
+// var c = "Yah!  I can write  HTML content through JavaScript"
+// document.write(c)
+
+// //   ==== " chp 02  Task No 09"
+// var c = "“▬▬▬▬▬▬▬▬▬ஜ ۩۞۩ ஜ▬▬▬▬▬▬▬▬▬”";
+// alert(c)
+
+// ======   "Chapter 03" ====
+
+// //   ==== " chp 03 Task No 01"
+// var age = "I am 26 Year Old";
+// alert(age)
+
+// //   ==== " chp 03 Task No 02"
+// Not understand //
+
+// //   ==== " chp 03 Task No 03"
+// var birthyear = "My birth year is 1994 <br>  Data type of my declared variable";
+// document.write(birthyear)
+
+// //   ==== " chp 03 Task No 04"
+// var visit = "“www.onlinecloth.com”"
+// document.write(visit)
+
+// var visitorname = "Visitor's Name <b> : Usman </b> <br> Product title   : <b> T-shirt </b> <br> Quantity : <b> 05 </b>   "
+// document.write(visitorname) 
+
+// var visitorname = " <b> Usman </b>  ordered <b> 5 T-shirt(s) </b> on Online Clothing store"
+// document.write(visitorname) 
+
+// ======   "Chapter 04" ====
+
+// //   ==== " chp 04 Task No 01"
+// var a = "var let const"
+// document.write(a)
+
+// //   ==== "chp 04  Task No 02"
+// var legal = "legal variable name<br> <li>letters </li> <li> number </li> <li> dollar </li> <li> signs  </li> <li> underscores</li>"
+// document.write(legal)
+
+// var illegal = "illegal variable name<br> <li>starting with number </li> <li> is a JavaScript reserved keyword </li> <li> has  spaces in name</li> <li> contain ampersand </li>"
+// document.write(illegal)
+
+
+// //   ==== "chp 04  Task No 03"
+// var a = " <b>“ Rules for naming JS variables ”</b> <br> <br><br> <br> Variable names can only contain ______, ______, ______ and ______. For example $my_1stVariable <br> Variables must begin with a ______, ______ or _____. For example $name, _name or name <br>  Variable names are case _________ <br> Variable names should not be JS _________"
+// document.write(a)
+
+
+// ======   "Chapter 05" ====
+
+// //   ==== "chp 05  Task No 01"
+
+// var a = 3;
+// var b = 5;
+// var c = a+b;
+// document.write(c)
+
+// var d = "Sum of 3  and 5  is 8"
+// document.write(d)
+ 
+// //   ==== " chp 05  Task No 02"
+
+// var a = 15;
+// var b = 5;
+// var c = a - b;
+// document.write(c)
+
+// var d = "Substraction of 15 and 5  is 10"
+// document.write(d)
+
+// var a = 5;
+// var b = 5;
+// var c = a * b;
+// document.write(c)
+
+// var d = "Multiplication of 5 and 5  is 25"
+// document.write(d)
+
+// var a = 25;
+// var b = 5;
+// var c = a / b;
+// document.write(c)
+
+// var d = "Division of 25 and 5  is 5"
+// document.write(d)
+
+// var a = 10;
+// var b = 5;
+// var c = 7;
+// var d = a + b * c;
+// document.write(c)
+
+// var d = "Modulus of 7 * 5 and + 10 is 45"
+// document.write(d)
+
+// //   ==== " chp 05  Task No 03"
+
+// //   ==== "Task No 03   Part A"
+// document.write("Value after variable declaration is undefined")
+
+// //   ==== "chp 05  Task No 03   Part B"
+// var a ="“Value after variable declaration is ”"
+// var b =5
+// var c = a + b
+// document.write(c)
+
+// //   ==== "chp 05   Task No 03   Part C and D"
+// document.write("“Initial value 5”")
+
+// //   ==== "chp 05   Task No 03   Part E and F"
+// var a = 1
+// var b = 5
+// var c = a + b
+// document.write(c)
+// var c = "“Value after increment is 6”"
+// document.write(c)
+
+// //   ==== "chp 05   Task No 03   Part G and H"
+// var a = 7
+// var b = 6
+// var c = a + b  
+// document.write(c)
+
+// var a = "“Value after addition is: 13”"
+// document.write(a)
+
+// //   ==== "chp 05   Task No 03   Part I and J"
+// var a = 13
+// var b = 1
+// var c = a - b  
+// document.write(c)
+
+// var c = "“Value after decrement is : 12”"
+// document.write(c)
+
+// //   ==== "chp 05  Task No 03   Part K and L"
+// var a = 12
+// var b = 4
+// var c = a / b  
+// document.write(c)
+
+// document.write("The Remainder is : 0")
 
 
 
- alert("Hellow... I can run JS though my web brower's console")
+
+
+// //   ==== "chp 05   Task No 04"
+
+
+// var price = 600;
+// var buy =  5
+// var total = price * buy
+// document.write(total)
+// document.write("Total cost to buy 5 tickets to a movie is 3000PKR")
+
+// //   ==== "chp 05   Task No 05"
+
+// document.write("Table of 5 <br> 5 x 1 = 5 <br> 5 x 2 = 10 <br> 5 x 3 = 15 <br> 5 x 4 = 20 <br> 5 x 5 = 25 <br> 5 x 6 = 30 <br> 5 x 7 = 35 <br> 5 x 8 = 40 <br> 5 x 9 = 45 <br> 5 x 10 = 50")
+
+
+// //   ==== "chp 05   Task No 06"
+/// skip
 
 
 
-// Track 02
-var username=("username")
-alert(username)
+// //   ==== "chp 05  Task No 07"
+// document.write(" <h1>  Shopping  Cart  </h1>")
+// var a = "Price Of item 1 is 650 <br>"
+// document.write(a)
+// var b =  "Quantity of item 1 is 3 <br>"
+// document.write(b)
+// var c = "Price Of item 2 is 100 <br>"
+// document.write(c)
+// var d = "Quantity of item 2 is 7 <br>"
+// document.write(d)
+// var e = "Shipping Charges  100 <br> <br>"
+// document.write(e)
 
-var myname = ("Mohammad Usman")  
-alert(myname)
+// document.write("Total cost of your order is 2750")
+
+// //   ==== "chp 05  Task No 08"
+// document.write("<h1>  Marks Sheet  </h1> <br> ")
+// document.write("Total marks : 980 <br> ")
+// document.write("Marks obtain : 804 <br> ")
+
+// document.write("Percentage :")
+// var obtainmarks = "804"
+// var totalmarks = "980"
+// var c = obtainmarks / totalmarks * 100
+// document.write(c)
 
 
-var a = 26
-var b = "year old"
-var c = a + b
-alert(c)
+// //   ==== "chp 05  Task No 09"
+// document.write("<h1> Currency in PKR  </h1> <br>")
+// document.write("Total Currency in PKR:")
+// var a = 10 
+// var b = 25
+// var c = 104.80
+// var d = 28 
+// var e = a * c + b * d 
+// document.write(e)
 
-alert("Certified  Mobile Application development")
+// //   ==== "chp 05  Task No 10"
+
+// var a = 5;
+// var b = 5;
+// var c = 5;
+// var d = (a + b) / c;
+// document.write(d)
+
+// //   ==== "chp 05  Task No 11"
+
+// document.write("<h1> Age Calculator </h1>")
+// document.write("Current Year : 2020 <br> Birth Year:1994 <br> ")
+
+// var b = + prompt ("Enter Current Year")
+// var a = + prompt ("Enter Your Birth Year")
+// var c = "Your Age is "
+// var d =  c + ( b - a ) 
+// document.write(d)
 
 
-var withBreaks = "PIZZA\nPIZZ\nPIZ\nPI\nP";
-alert(withBreaks);
+
+// ======   "Chapter 06 to 09" ====
+
+// //   ==== "chp 6 to 9  Task No 1"
+
+// var a = 10
+// var b = "The value of a is:"
+// var c = b + a 
+// document.write(c); 
+// document.write("<br> <br>")
+// var a = 10
+// var b = "The value of ++a is: "
+// var c = b +  (a + + 1)
+// document.write(c) 
+// document.write("<br>Now the value Of a is: 11 <br> <br>")
+// var a = 10
+// var b = "The value of a++ is: "
+// var c = b +  (a + + 1)
+// document.write(c) 
+// document.write("<br>Now the value Of is: 12 <br> <br>")
+// var a = 10
+// var b = "The value of --a is: "
+// var c = b +  (a - -  1)
+// document.write(c) 
+// document.write("<br>Now the value Of is: 11 <br> <br>")
+// var a = 10
+// var b = "The value of a-- is: "
+// var c = b +  (a - - 1)
+// document.write(c) 
+// document.write("<br>Now the value Of is: 10 <br> <br>")
+
+// //   ==== "chp 6 to 9 Task No 2"
+// document.write("a is : 2 <br>  b is : 1 <br>")
+// // var a = 2
+// // var b = 1
+// // var c = --a + b;
+// // document.write(,a)
+
+// // var a = 2
+// // var b = 1
+// // var c = --a - --b;
+// // document.write(c)
+
+// // var a = 2
+// // var b = 1
+// // var c = --a - --b + ++b;
+// // document.write(c)
+
+// var a = 2
+// var b = 1
+// var c = --a - --b + ++b  + b;
+// document.write("result is : ", c)
+
+
+// //   ==== "chp 6 to 9 Task No 3"
+
+// var a =  prompt ("Enter your name")
+// var b = "Welcome to "
+// var c = b + a;
+// alert(c)
+
+// //   ==== "chp 06 Task No 5"
+// var b = + prompt("Enter Number")
+// for(var a = + 1 ; a <= 10; a++){
+//     document.write(b + "x" + a + "=" + b*a + "<br>")
+// }
+
+// //   ==== "chp 06  Task No 6 to 9"
+// document.write(" <table> <tr> <td> <h6> Subject </h6> </td> <td> <h6> TotalMarks </h6> </td> <td> <h6> ObtainMarks </h6> </td> <td> <h6> Percentage</h6> </td> </tr> </table>  ")
+
+// var a = "English"; 
+// var b = 100;
+// var c = 54 ;
+// var d = a + b + c 
+// document.write(" ", " ",  d ," ", " ", " 54%", "<br>" )
+
+// var a = + prompt ("Enter Math Marks")
+// var b = 100
+// var c = a / b * 100
+// document.write ("Math  100", " ", " ", " ",  " ", a, " ", " ",   c  , "%", "<br>")     
+ 
+// var a = + prompt ("Enter Urdu Marks")
+// var b = 100
+// var c = a / b * 100
+
+// document.write ("Urdu  100", " ", " ", " ",  " ", a, " ", " ",   c  , "%", "<br>" )  
+
+
+// ======   "Chapter 09 to 11" ====
+
+// //   ==== "chp 09 to 11 Task No 1"
+
+
+// var a =  prompt("Enter City")
+// if (a == "karachi"){
+//     alert("Welcome to city of lights")
+
+// }
+// else {
+//     alert("Welcome")
+// }
+
+// //   ==== "chp 09 to 11 Task No 02"
+
+// var a =  prompt("Enter Gender")
+// if (a == "male"){
+//     alert("Good Morning Sir")
+
+// }
+// else if (a == "female"){
+//     alert("Good Morning Ma'm.")
+
+// }
+// else {
+//     alert("Good Morning")
+// }
+
+// //   ==== "chp 09 to 11 Task No 04"
+
+// var a = + prompt("Enter remaining fuel in a car")
+// if (a  <= 0.25 ){
+//     alert("Please refill the fuel in your car")
+
+// }
+// else {
+//     alert(a)
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
